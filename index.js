d22ef3d1b3fda148b285bb9937f5779e59d44938
Evan Pavone <evanrpavone@gmail.com>
2020-07-24 14:52:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,7 +40,11 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++){
+    if (item === cart[i].itemName) {
+      cart.splice(i, 1); //removes 1 item at index i
+      return cart;
+    }
 }
 
 function placeOrder(cardNumber) {
